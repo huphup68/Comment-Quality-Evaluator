2bd15128e91b5713e1cf653fa2e711c43cc97752
Alexander Gustafsson <gusgusalh@student.gu.se>
2018-04-11 11:58:17 +0200

    Added basic readability calculations

---
+++ src/comments/Readability.java
@@ -0,0 +1,61 @@
+package comments;
+import edu.stanford.nlp.simple.*;
+
+public class Readability {
+	private String comment;
+	private Document doc;
+	private int sentenceCount = 0;
+	private int complexWords = 0;
+	private int wordCount = 0;
+	private String[] words = {"a", "b", "c", "d", "e", "f"};
+	public Readability(String comment) {
+		this.comment = comment;
+		doc = new Document(this.comment);
+		for(Sentence sent : doc.sentences()) {
+			sentenceCount++;
+			for(String word : sent.words()) {
+				wordCount++;
+			}
+		}
+	}
+	/** 
+	 * Searches the array of words to see if the examined word is included
+	 * @return True if the word examined exists in the array, otherwise false 
+	 * */
+	private boolean binarySearch(String exWord) {
+		int bot = 0;
+		int top = wordCount;	
+		int mid;
+		while(bot <= top) {
+			mid = (top + bot)/2;
+			if(exWord.compareToIgnoreCase(words[mid]) < 0) {
+				top = mid -1;
+			}
+			else if(exWord.compareToIgnoreCase(words[mid]) > 0) { 
+				bot = mid +1;
+			}
+			else {
+				return true;
+			}
+		}
+		return false;
+	}
+	private double daleChallCalculator() {
+		double res;
+		res = 0.157*((double)complexWords/(double)wordCount*100) + (0.0496*((double)wordCount/(double)sentenceCount));
+		if((double)complexWords/(double)wordCount > 0.05) {
+			res += 3.6365;
+		}
+		return res;
+	}
+	public double daleChall(){
+		for(Sentence sent : doc.sentences()) {
+			for(String word : sent.words()) {
+				if(!binarySearch(word)) {
+					complexWords++;
+				}
+			}
+		}
+		return daleChallCalculator();
+	}
+}
