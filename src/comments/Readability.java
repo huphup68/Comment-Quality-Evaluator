37dbea810052a4ef6944fbb8f3b9a036c695822d
Alexander Gustafsson <gusgusalh@student.gu.se>
2018-04-23 13:40:29 +0200

    Readability no longer needs to be instatiated

--- src/comments/Readability.java
+++ src/comments/Readability.java
@@ -1,37 +1,24 @@
 package comments;
-import alex.myTest.Concurrency;
-import alex.myTest.List;
+
 import edu.stanford.nlp.simple.*;
 
-public class Readability {
-	private String comment;
-	private Document doc;
-	private int sentenceCount = 0;
-	private int complexWords = 0;
-	private int wordCount = 0;
-	public Readability(String comment) {
-		this.comment = comment;
-		doc = new Document(this.comment);
-		for(Sentence sent : doc.sentences()) {
-			sentenceCount++;
-			for(String word : sent.words()) {
-				wordCount++;
-			}
-		}
+public final class Readability {
+	private Readability() {
+		
 	}
 	/** 
 	 * Searches the array of words to see if the examined word is included
 	 * @return True if the word examined exists in the array, otherwise false 
 	 * */
-	private boolean binarySearch(String exWord) {
+	private static boolean binarySearch(String exWord) {
 		int bot = 0;
-		int top = wordCount;	
+		int top = DaleChallWords.getDaleChall().length;	
 		int mid;
 		while(bot <= top) {
 			mid = (top + bot)/2;
 			if(Concurrency.levenshtein(DaleChallWords.getDaleChall()[mid], exWord) > 2) {
 				if(exWord.compareToIgnoreCase(DaleChallWords.getDaleChall()[mid]) < 0) {
-					top = mid -1;
+						top = mid -1;
 				}
 				else if(exWord.compareToIgnoreCase(DaleChallWords.getDaleChall()[mid]) > 0) { 
 					bot = mid +1;
@@ -47,7 +34,7 @@ public class Readability {
 	 * Applies the formula for calculating the Dale-Chall sore to the comment
 	 * @return The Dale-Chall score of the comment 
 	 * */
-	private double daleChallCalculator() {
+	private static double daleChallCalculator(int complexWords, int wordCount, int sentenceCount) {
 		double res;
 		res = 0.157*((double)complexWords/(double)wordCount*100) + (0.0496*((double)wordCount/(double)sentenceCount));
 		if((double)complexWords/(double)wordCount > 0.05) {
@@ -59,14 +46,20 @@ public class Readability {
 	 * Iterates over each word to count how many are complex and then returns score
 	 * @return The Dale-Chall score of the comment
 	 */
-	public double daleChall(){
+	public static double daleChall(String text){
+		int complexWords = 0;
+		int wordCount = 0;
+		int sentenceCount = 0;
+		Document doc = new Document(text);
 		for(Sentence sent : doc.sentences()) {
+			sentenceCount++;
 			for(String word : sent.words()) {
+				wordCount++;
 				if(!binarySearch(word)) {
 					complexWords++;
 				}
 			}
 		}
-		return daleChallCalculator();
+		return daleChallCalculator(complexWords, wordCount, sentenceCount);
 	}
 }
